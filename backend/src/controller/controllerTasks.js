4cb3727099b28cb26f65e9b36998d0d04b40672c
reinaldoper <reinaldoper83@gmail.com>
2025-02-01 09:12:02 -0400

    fix: Fix controller and your responses.

--- backend/src/controller/controllerTasks.js
+++ backend/src/controller/controllerTasks.js
@@ -17,10 +17,25 @@ class ControllerTasks {
     return res.status(statusCode.CREATED).json({ message: newTask });
   }
 
+  updateTaskStatus = (req, res) => {
+    const { id } = req.params;
+    const { status: taskStatus } = req.body;
+    const task = this.service.getTaskById(id);
+    if (task.error) {
+      return res.status(statusCode.NOT_FOUND).json({ error: task.error });
+    }
+    const updatedTask = this.service.updateTaskStatus({ ...task, status: taskStatus }, Number(id));
+    if (updatedTask.error) {
+      return res.status(statusCode.BAD_REQUEST).json({ error: task.error });
+    }
+    
+    return res.status(statusCode.OK).json({ message: updatedTask });
+  }
+
   updateTask = (req, res) => {
     const { id } = req.params;
     const task = req.body;
-    const updatedTask = this.service.updateTask(task, id);
+    const updatedTask = this.service.updateTask(task, Number(id));
     if (updatedTask.error) {
       return res.status(statusCode.NOT_FOUND).json({ error: task.error });
     }
