092ce2ecc6f97371f4ecc7f1fcf081b5c49f9eae
reinaldoper <reinaldoper83@gmail.com>
2025-02-01 01:21:00 -0400

    feat: Add service tasks and your task controller in memory.

---
+++ backend/src/service/serviceTasks.js
@@ -0,0 +1,49 @@
+class ServiceTasks {
+  tasks = [];
+  id = 1;
+
+  constructor() {}
+
+  addTask = (task) => {
+    task.id = this.id++;
+    this.tasks.push(task);
+    return task;
+  }
+
+  getAllTasks = () => {
+    return this.tasks;
+  }
+
+  updateTask = (task, id) => {
+    const index = this.tasks.findIndex((t) => t.id === Number(id));
+    if (index === -1) {
+      return { error: "Tarefa não encontrada." };
+    }
+    task.id = this.tasks[index].id;
+    this.tasks[index] = task;
+    return task;
+  }
+
+  deleteTask = (id) => {
+    const index = this.tasks.findIndex((t) => t.id === Number(id));
+    console.log(index);
+    
+    if (index === -1) {
+      return { error: "Tarefa não encontrada." };
+    }
+    this.tasks.splice(index, 1);
+    return { message: "Tarefa deletada com sucesso." };
+  }
+
+  getTaskById = (id) => {
+    const index = this.tasks.findIndex((t) => t.id === Number(id));
+    
+    if (index === -1) {
+      return { error: "Tarefa não encontrada." };
+    }
+    const getTask = this.tasks[index];
+    return getTask;
+  }
+}
+
+export default ServiceTasks;
