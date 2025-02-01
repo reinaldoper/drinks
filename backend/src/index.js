eb859c6a01b7f67d91e6e983b8892530b0875c45
reinaldoper <reinaldoper83@gmail.com>
2025-02-01 09:11:21 -0400

    fix: Adjust the server with endpoints.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import router from './routes/taskRoute.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
