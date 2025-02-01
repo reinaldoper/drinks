91bed4c9001a7fdfc6e068d6dcdf1d95452e5e58
reinaldoper <reinaldoper83@gmail.com>
2025-02-01 09:13:02 -0400

    fix: Adjust middleware for handle with responses.

--- backend/src/middleware/task.js
+++ backend/src/middleware/task.js
@@ -2,21 +2,21 @@ import statusCode from "../utils/statusCode.js";
 export const PostTask = (req, res, next) => {
   const { titulo, descricao } = req.body;
   if (!titulo || !descricao || titulo.length <= 4 || descricao.length <= 4) {
-    return res.status(statusCode.BAD_REQUEST).json({ message: 'Invalid data' });
+    return res.status(statusCode.BAD_REQUEST).json({ error: 'Invalid data' });
   }
   next();
 };
 
 
 export const PutTask = (req, res, next) => {
-  const { titulo, descricao, status } = req.body;
+  const { titulo, descricao } = req.body;
   const { id } = req.params;
   const idNumber = Number(id);
-  if (!titulo || !status || !descricao || titulo.length <= 4 || descricao.length <= 4 || status.length <= 4) {
-    return res.status(statusCode.BAD_REQUEST).json({ message: 'Invalid data' });
+  if (!titulo || !descricao || titulo.length <= 4 || descricao.length <= 4) {
+    return res.status(statusCode.BAD_REQUEST).json({ error: 'Invalid data' });
   }
   if (!idNumber) {
-    return res.status(statusCode.BAD_REQUEST).json({ message: 'Invalid data' });
+    return res.status(statusCode.BAD_REQUEST).json({ error: 'Invalid data' });
   }
   next();
 };
@@ -25,7 +25,15 @@ export const TaskId = (req, res, next) => {
   const { id } = req.params;
   const idNumber = Number(id);
   if (!idNumber) {
-    return res.status(statusCode.BAD_REQUEST).json({ message: 'Invalid data' });
+    return res.status(statusCode.BAD_REQUEST).json({ error: 'Invalid data' });
+  }
+  next();
+};
+
+export const TaskStatus = (req, res, next) => {
+  const { status } = req.body;
+  if (!status) {
+    return res.status(statusCode.BAD_REQUEST).json({ error: 'Invalid data' });
   }
   next();
 };
