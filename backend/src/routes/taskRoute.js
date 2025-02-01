9e7c86a85da92389f4420db99c9b06e76c110dd4
reinaldoper <reinaldoper83@gmail.com>
2025-02-01 09:13:34 -0400

    fix: Fix routes and add new routes

--- backend/src/routes/taskRoute.js
+++ backend/src/routes/taskRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import ControllerTasks from '../controller/controllerTasks.js';
-import { PostTask, PutTask, TaskId } from '../middleware/task.js'
+import { PostTask, PutTask, TaskId, TaskStatus } from '../middleware/task.js'
 
 const tasks = new ControllerTasks();
 
@@ -8,8 +8,9 @@ const router = express.Router();
 
 router.get('/task', tasks.getAllTasks);
 router.post('/task', PostTask, tasks.addTask);
-router.put('/task/:id', PutTask, tasks.updateTask);
+router.put('/task/:id', TaskId, PutTask, tasks.updateTask);
 router.delete('/task/:id', TaskId, tasks.deleteTask);
 router.get('/task/:id', TaskId, tasks.getTaskById);
+router.put('/task/status/:id', TaskId, TaskStatus, tasks.updateTaskStatus);
 
 export default router;
