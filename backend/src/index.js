ff53d4d6f2bf2875f356137fb659d4ab484323e0
reinaldoper <reinaldoper83@gmail.com>
2025-02-01 01:22:36 -0400

    feat: Server init.

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import router from './routes/taskRoute.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
